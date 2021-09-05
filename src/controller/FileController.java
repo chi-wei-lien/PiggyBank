860912ea816cc25ec81e55349b95a06a0d98082a
chi-wei-lien <lien2@purdue.edu>
2021-09-05 10:48:55 -0400

    seperate dataNume (used as order of objects in dictionary) to expenseDataNum and incomeDataNum.

--- src/controller/FileController.java
+++ src/controller/FileController.java
@@ -16,20 +16,24 @@ public class FileController {
         try{
             File dataFile = new File("data.txt");
             Scanner reader = new Scanner(dataFile);
-            int dataNum = 0;
+            int expenseDataNum = 0;
+            int incomeDataNum = 0;
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
                 String dataDetail = data.substring(data.indexOf("s") + 2);
+//                int debug = data.indexOf("e");
+//                System.out.println(debug);
                 if(data.indexOf("e") < 1){
                     String moneyStr = data.substring(data.indexOf("e") + 2, data.indexOf(" "));
-                    expenseDataSet.put(dataNum, new Expense(Double.parseDouble(moneyStr), dataDetail));
+                    expenseDataSet.put(expenseDataNum, new Expense(Double.parseDouble(moneyStr), dataDetail));
 //                    System.out.println("e: " + data.substring(data.indexOf("e") + 2, data.indexOf(" "))); //2 is for the character after e (e: )
+                    expenseDataNum++;
                 } else {
                     String moneyStr = data.substring(data.indexOf("i") + 2, data.indexOf(" "));
-                    incomeDataSet.put(dataNum, new Income(Double.parseDouble(moneyStr), dataDetail));
+                    incomeDataSet.put(incomeDataNum, new Income(Double.parseDouble(moneyStr), dataDetail));
 //                    System.out.println("i: " + data.substring(data.indexOf("i") + 2, data.indexOf(" "))); //2 is for the character after i (i: )
+                    incomeDataNum++;
                 }
-                dataNum++;
             }
         }catch(IOException error){
             System.out.println("System failed to read info from data.txt");
