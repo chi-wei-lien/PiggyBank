4b7cc6e70f84290e658d00fec1a9665f7786c7d3
chi-wei-lien <lien2@purdue.edu>
2021-09-05 10:45:24 -0400

    update loadData function

--- src/controller/Data.java
+++ src/controller/Data.java
@@ -1,8 +1,18 @@
 package controller;
 
+import datatemplate.*;
+
+
 public class Data {
     public static void loadData(){
-        System.out.println(FileController.expenseDataSet.get(1).getExpense());
-        System.out.println(FileController.incomeDataSet.get(1).getIncome());
+        System.out.println(FileController.expenseDataSet);
+        for(int i = 0; i < FileController.expenseDataSet.size(); i++){
+            Expense expense = FileController.expenseDataSet.get(i);
+            System.out.println("expense: " + expense.getExpense() + "; data: " + expense.getDate() + "; detail: " + expense.getDetail());
+        }
+        for(int i = 0; i < FileController.incomeDataSet.size(); i++){
+            Income income = FileController.incomeDataSet.get(i);
+            System.out.println("income: " + income.getIncome() + "; data: " + income.getDate() + "; detail: " + income.getDetail());
+        }
     }
 }
