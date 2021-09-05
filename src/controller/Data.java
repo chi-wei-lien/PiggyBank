d010ae285e26ff47419497e320cdd6f462ea8a41
chi-wei-lien <lien2@purdue.edu>
2021-09-05 11:01:05 -0400

    [typo] data -> date

--- src/controller/Data.java
+++ src/controller/Data.java
@@ -8,11 +8,11 @@ public class Data {
         System.out.println(FileController.expenseDataSet);
         for(int i = 0; i < FileController.expenseDataSet.size(); i++){
             Expense expense = FileController.expenseDataSet.get(i);
-            System.out.println("expense: " + expense.getExpense() + "; data: " + expense.getDate() + "; detail: " + expense.getDetail());
+            System.out.println("expense: " + expense.getExpense() + "; date: " + expense.getDate() + "; detail: " + expense.getDetail());
         }
         for(int i = 0; i < FileController.incomeDataSet.size(); i++){
             Income income = FileController.incomeDataSet.get(i);
-            System.out.println("income: " + income.getIncome() + "; data: " + income.getDate() + "; detail: " + income.getDetail());
+            System.out.println("income: " + income.getIncome() + "; date: " + income.getDate() + "; detail: " + income.getDetail());
         }
     }
 }
